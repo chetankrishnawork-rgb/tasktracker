10dc37eda35ec59f0f52ec1b808a5f78795eac1b
chetankrishnawork-rgb <chetan.krishna.work@gmail.com>
2026-07-27 21:15:54 -0700

    Add IconBell for the due-today notifications toggle

--- lib/icons.tsx
+++ lib/icons.tsx
@@ -181,3 +181,12 @@ export function IconBarChart(props: IconProps) {
     </svg>
   );
 }
+
+export function IconBell(props: IconProps) {
+  return (
+    <svg {...base} {...props}>
+      <path d="M6 8a6 6 0 0 1 12 0c0 5 2 6 2 6H4s2-1 2-6" />
+      <path d="M10 21a2 2 0 0 0 4 0" />
+    </svg>
+  );
+}
